be952033176d25e87b07711013a556bd6dce809c
hobbyquaker <hq@ccu.io>
2017-03-01 14:58:11 +0100

    add button node

---
+++ nodes/paper-button.js
@@ -0,0 +1,31 @@
+module.exports = function(RED) {
+
+    var ui = require('../ui')(RED);
+
+    function PolymerPaperButtonNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        var done = ui.add({
+            node: node,
+            control: {
+                type:           'polymer_paper-button',
+                parent:         config.parent,
+                element:        'paper-button',
+                html:           config.html,
+                attributes:     config.attributes,
+                valueAttribute: 'active',
+                event:          'click',
+                width:          config.width,
+                height:         config.height,
+                topic:          config.topic,
+                payload:        config.payload,
+                payloadType:    config.payloadType,
+            }
+        });
+
+        node.on("close", done);
+    }
+
+    RED.nodes.registerType("polymer_paper-button", PolymerPaperButtonNode);
+};
