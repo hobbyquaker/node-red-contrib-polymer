125ae0a83c224fc4b7a0e3ee5fbda9eee2fcb2bc
hobbyquaker <hq@ccu.io>
2017-03-07 21:54:52 +0100

    add external img/iframe widget

---
+++ nodes/polymer_external.js
@@ -0,0 +1,28 @@
+module.exports = function(RED) {
+
+    var ui = require('../ui')(RED);
+
+    function PolymerExternal(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        var done = ui.add({
+            node: node,
+            control: {
+                type:           'polymer_external',
+                parent:         config.parent,
+                element:        'node-red-external',
+                'src-interval': config.srcInterval,
+                'src-url':      config.srcUrl,
+                'src-type':     config.srcType,
+                attrs:          ['src-url', 'src-type', 'src-interval'],
+                width:          config.width,
+                height:         config.height
+            }
+        });
+
+        node.on("close", done);
+    }
+
+    RED.nodes.registerType("polymer_external", PolymerExternal);
+};
